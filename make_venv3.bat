d4c1021def3e2f4914b6c69a326bf44f6681b972
james abel <j@abel.co>
2017-12-30 02:24:10 -0800

    add AWS upload

---
+++ make_venv3.bat
@@ -0,0 +1,5 @@
+REM Python 3 version for boto3
+"C:\Program Files\Python36\python.exe" -m venv --clear venv3
+venv3\Scripts\pip install -U pip
+venv3\Scripts\pip install -U setuptools
+venv3\Scripts\pip install -r requirements.txt
---
+++ upload_to_aws.py
@@ -0,0 +1,35 @@
+
+import os
+import glob
+
+import boto3
+
+from keys import aws_abel_co
+
+
+def main():
+    s3_resource = boto3.resource('s3', aws_access_key_id=aws_abel_co['Access key ID'],
+                  aws_secret_access_key=aws_abel_co['Secret access key'],
+                  region_name='us-west-2')
+    s3_client = boto3.client('s3', aws_access_key_id=aws_abel_co['Access key ID'],
+                aws_secret_access_key=aws_abel_co['Secret access key'],
+                region_name='us-west-2')
+
+    build_dir = '_build'
+    bucket = 'practicalprojectexecution'
+    for file_path in glob.glob(os.path.join(build_dir, '**'), recursive=True):
+        if os.path.isfile(file_path):
+            key = file_path.replace(build_dir, '')
+            if key.startswith(os.sep):
+                key = key[1:]
+            key = key.replace(os.sep, '/')
+            print(file_path, key)
+            # todo: check if changed before bothering to upload
+            s3_resource.meta.client.upload_file(file_path, bucket, key,
+                                                ExtraArgs={'ContentType': 'text/html', 'ACL': "public-read"})
+
+    print('https://s3-us-west-2.amazonaws.com/practicalprojectexecution/index.html')
+
+
+if __name__ == '__main__':
+    main()
